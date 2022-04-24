84ca69fc377a49869587272ffa940933f27f46c3
xwj <1983318249@qq.com>
2022-04-24 21:21:55 +0800

    jdbc

--- jdbc/src/JDBC.java
+++ jdbc/src/JDBC.java
@@ -3,6 +3,7 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -54,19 +55,97 @@ public class JDBC {
         return result;
     }
 
+    public static int deleteMySQL(int id) throws SQLException {
+        DataSource dataSource = new MysqlDataSource();
+        ((MysqlDataSource)dataSource).setUrl("jdbc:mysql://127.0.0.1:3306/test?characterEncoding=utf8&useSSL=false");
+        ((MysqlDataSource)dataSource).setUser("root");
+        ((MysqlDataSource)dataSource).setPassword("1234");
+
+        Connection connection = dataSource.getConnection();
+
+        String sql = "delete from subject where id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setInt(1,id);
+        System.out.println(statement);
+
+        int result = statement.executeUpdate();
+
+        statement.close();
+        connection.close();
+
+        return result;
+    }
+
+    public static int updateMySQL(int id,String newName) throws SQLException {
+        DataSource dataSource = new MysqlDataSource();
+
+        ((MysqlDataSource)dataSource).setUrl("jdbc:mysql://127.0.0.1:3306/test?characterEncoding=utf8&useSSL=false");
+        ((MysqlDataSource)dataSource).setUser("root");
+        ((MysqlDataSource)dataSource).setPassword("1234");
+
+        Connection connection = dataSource.getConnection();
+
+        String sql = "update subject set name = ? where id = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1,newName);
+        preparedStatement.setInt(2,id);
+
+        System.out.println(preparedStatement);
+
+        int result = preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+
+        return result;
+    }
+
+    public static void selectMySQL() throws SQLException {
+        DataSource dataSource = new MysqlDataSource();
+
+        ((MysqlDataSource)dataSource).setURL("jdbc:mysql://127.0.0.1:3306/test?characterEncoding=utf8&useSSL=false");
+        ((MysqlDataSource)dataSource).setUser("root");
+        ((MysqlDataSource)dataSource).setPassword("1234");
+
+        Connection connection = dataSource.getConnection();
+
+        String sql = "select * from subject";
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            int credit = resultSet.getInt("credit");
+            System.out.println("id="+id+",name="+name+",credit="+credit);
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入id>>");
         int id  = scanner.nextInt();
-        System.out.println("请输入name>>");
-        String name = scanner.next();
-        System.out.println("请输入credit>>");
-        int credit = scanner.nextInt();
+//        System.out.println("请输入name>>");
+//        String name = scanner.next();
+//        System.out.println("请输入credit>>");
+//        int credit = scanner.nextInt();
 
         //将输入的值添加到数据库中
         try {
-            int result = insertMySQL(id, name, credit);
-            System.out.println(result);
+//            int result = insertMySQL(id, name, credit);
+//            int result = updateMySQL(id,"盘古大神");
+//            int result = deleteMySQL(id);
+//            System.out.println(result);
+            selectMySQL();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("插入失败!!!");
