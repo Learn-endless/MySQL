8bae1544daa6f2be4fd99c9d7fbbbb57cd102c03
xwj <1983318249@qq.com>
2022-06-21 22:07:21 +0800

    -

---
+++ 2022_5_29/src/com/demo21/Test.java
@@ -0,0 +1,12 @@
+package com.demo21;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        Hashtable hashtable = new Hashtable();
+    }
+}
